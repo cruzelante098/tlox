c0baf3ed6c59c0b8e3791e69b23a1fa9f9b636fd
Francisco Cruz <cruzelante098@gmail.com>
2019-05-27 21:11:30 +0100

    Create a function to scan, parse and interpret lox expressions

--- test/interpreter-test.ts
+++ test/interpreter-test.ts
@@ -7,16 +7,19 @@ import { Parser } from '../src/parser';
 
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-describe('Interpreter', () => {
-  let scanner = new Scanner();
-  let parser = new Parser();
-  let interpreter = new Interpreter();
+const scanner = new Scanner();
+const parser = new Parser();
+const interpreter = new Interpreter();
 
+describe('Interpreter', () => {
   it('is capable of parsing a simple expression', () => {
-    const source = '9 + 6 + 5';
-    const tokens = scanner.scan(source);
-    const ast = parser.parse(tokens);
-    const result = interpreter.interpret(ast!);
+    const result = execute('9 + 6 + 5');
     expect(result).to.be.equal('20');
   });
 });
+
+function execute(source: string): string | void {
+  const tokens = scanner.scan(source);
+  const ast = parser.parse(tokens);
+  return interpreter.interpret(ast!);
+}
